ec9dae27a72a5b61726e54445a68323e205fb9bd
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2020-04-02 13:49:45 +0200

    Update to Kotlin 1.3.70
    Renamed 'buildMap' to 'buildMutableMap' to avoid confusion with the new library function

--- Builders/String and map builders/src/Task.kt
+++ Builders/String and map builders/src/Task.kt
@@ -1,13 +1,13 @@
 import java.util.HashMap
 
-fun <K, V> buildMap(build: HashMap<K, V>.() -> Unit): Map<K, V> {
+fun <K, V> buildMutableMap(build: HashMap<K, V>.() -> Unit): Map<K, V> {
     val map = HashMap<K, V>()
     map.build()
     return map
 }
 
 fun usage(): Map<Int, String> {
-    return buildMap {
+    return buildMutableMap {
         put(0, "0")
         for (i in 1..10) {
             put(i, "$i")
