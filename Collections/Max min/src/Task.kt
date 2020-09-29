eb882a355eca6a76f9f239287f5fcc9b46170993
blueglyph <blueglyph@github.com>
2020-09-29 14:36:11 +0200

    Use count(), instead of size which forces to load the whole collection

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,6 +1,6 @@
 // Return a customer who has placed the maximum amount of orders
 fun Shop.getCustomerWithMaxOrders(): Customer? =
-        customers.maxBy { it.orders.size }
+        customers.maxBy { it.orders.count() }
 
 // Return the most expensive product that has been ordered by the given customer
 fun getMostExpensiveProductBy(customer: Customer): Product? =
