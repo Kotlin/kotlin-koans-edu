f795b61f36e1a445395b21ef071821adf2c939f2
Oleg Osipenko <oleg.s.osipenko@gmail.com>
2021-01-30 11:25:19 +0700

    Updated tasks to Kotlin 1.4

--- Collections/Getting used to new style/src/Task.kt
+++ Collections/Getting used to new style/src/Task.kt
@@ -2,7 +2,7 @@ fun doSomethingWithCollection(collection: Collection<String>): Collection<String
 
     val groupsByLength = collection.groupBy { s -> s.length }
 
-    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.max()
+    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.maxOrNull()
 
     return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
 }
