9da25866f2783fda17fc5c2d7c828a2c2815337b
Danil Pavlov <danil.pavlov@jetbrains.com>
2021-09-28 12:21:21 +0300

    fix: placeholder size and minor fixes

--- Collections/Fold/src/Task.kt
+++ Collections/Fold/src/Task.kt
@@ -1,9 +1,9 @@
 // Return the set of products that were ordered by all customers
 fun Shop.getProductsOrderedByAll(): Set<Product> {
     val allProducts = customers.flatMap { it.getOrderedProducts() }.toSet()
-    return customers.fold(allProducts, { orderedByAll, customer ->
+    return customers.fold(allProducts) { orderedByAll, customer ->
         orderedByAll.intersect(customer.getOrderedProducts())
-    })
+    }
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -1,9 +1,9 @@
-fun foo(name: String, number: Int = 42, upperCase: Boolean = false) =
-        (if (upperCase) name.uppercase() else name) + number
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.uppercase() else name) + number
 
 fun useFoo() = listOf(
         foo("a"),
         foo("b", number = 1),
-        foo("c", upperCase = true),
-        foo(name = "d", number = 2, upperCase = true)
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
 )
